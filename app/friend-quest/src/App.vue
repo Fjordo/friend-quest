824e671b3cf3c095492453479c841a28835899eb
Andrea Fiorucci <andrea.fiorucci@stepwise.it>
2023-04-18 17:11:11 +0200

    test

--- app/friend-quest/src/App.vue
+++ app/friend-quest/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloCopy msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HelloCopy from './components/HelloCopy.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HelloCopy
   }
 }
 </script>
